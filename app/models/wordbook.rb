5a1d5b1c2b48136d9af64a68302bda83569ce07d
maki-n <z16081nm@aiit.ac.jp>
2017-08-08 16:27:30 +0000

    英単語テストを追加

--- app/models/wordbook.rb
+++ app/models/wordbook.rb
@@ -1,9 +1,15 @@
 class Wordbook < ActiveRecord::Base
+  validates :japanese, length: { maximum: 10 }
+  validates :english, length: { maximum: 10 }
+
   def self.search(search)
     if search
       Wordbook.where(['english LIKE ?', "%#{search}%"])
     else
       Wordbook.all
     end
+  end
+  def self.quiz(quiz)
+    Wordbook.all
   end
 end
